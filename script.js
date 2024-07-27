44347e8b0309a4f32f405156e9ca6af6949d8396
Cedric <75742716+cedorikku@users.noreply.github.com>
2024-07-27 17:37:00 +0800

    Make canvas appear

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 const canvas = document.querySelector(".canvas");
 
 // TODO Create a 16x16 grid square w/ flexbox
+let gridSize = 16;
+
+function displayCanvas() {
+    for ( let i = 0; i < gridSize; i++ ) {
+        const row = document.createElement( "div" );
+        row.classList.add( "row" );
+        canvas.appendChild( row );
+
+        for ( let j = 0; j < gridSize; j++ ) {
+            const box = document.createElement( "span" );
+            row.appendChild( box );
+        }
+    }
+}
+
+displayCanvas();
 
 // TODO Add event listeners to these squares when hovering. Their colors change after being hovered.
 
