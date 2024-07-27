1ec397a1cca77cebbc1eb4eda913fd568192024b
Cedric <75742716+cedorikku@users.noreply.github.com>
2024-07-27 17:13:20 +0800

    Add todo for what to-do

---
+++ script.js
@@ -0,0 +1,7 @@
+const canvas = document.querySelector(".canvas");
+
+// TODO Create a 16x16 grid square w/ flexbox
+
+// TODO Add event listeners to these squares when hovering. Their colors change after being hovered.
+
+// TODO ...
